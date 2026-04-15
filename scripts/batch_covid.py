eaebb3b7a0c7e7fe726db9b76e99889063f5a990
Iván Andrés Olave Ramírez <127456620+Ivan-Olave@users.noreply.github.com>
2026-04-15 17:28:28 -0500

    Update batch_covid.py

--- scripts/batch_covid.py
+++ scripts/batch_covid.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-# Crear sesión
+# Creamos sesión
 spark = SparkSession.builder \
     .appName("Analisis COVID Colombia") \
     .getOrCreate()
 
-# Cargar CSV
+# Cargamos CSV
 df = spark.read.csv(
     "/home/vboxuser/tarea3_spark/data/covid19.csv",
     header=True,
@@ -19,7 +19,7 @@ df.printSchema()
 print("=== DATOS ===")
 df.show(5)
 
-# Limpiar datos
+# Limpiamos datos
 df_clean = df.select("Nombre departamento", "Estado") \
              .dropna(subset=["Nombre departamento", "Estado"])
 
@@ -33,14 +33,14 @@ df_dept = df_clean.groupBy("Nombre departamento").count()
 # Casos por estado
 df_estado = df_clean.groupBy("Estado").count()
 
-# Mostrar resultados
+# Mostramos resultados
 print("=== POR DEPARTAMENTO ===")
 df_dept.show()
 
 print("=== POR ESTADO ===")
 df_estado.show()
 
-# Guardar resultados
+# Guardamos resultados
 df_dept.coalesce(1).write.csv(
     "/home/vboxuser/tarea3_spark/output/departamentos",
     header=True,
